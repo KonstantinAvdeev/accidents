e1bd4f6e51f39510a94086900d3cc6cd9f7bf775
Konstantin Avdeev <fujiun91@gmail.com>
2023-05-17 14:58:17 +0300

    3.1. @RequestParam. Форма редактирования [#308708]

--- src/main/java/ru/job4j/accidents/controller/AccidentController.java
+++ src/main/java/ru/job4j/accidents/controller/AccidentController.java
@@ -3,10 +3,7 @@ package ru.job4j.accidents.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.job4j.accidents.model.Accident;
 import ru.job4j.accidents.service.AccidentService;
 
@@ -28,19 +25,19 @@ public class AccidentController {
         return "redirect:/index";
     }
 
-    @GetMapping("/{id}")
-    public String viewEditAccident(@PathVariable int id, Model model) {
+    @GetMapping("/editAccident")
+    public String viewEditAccident(@RequestParam int id, Model model) {
         Optional<Accident> optionalAccident = accidents.findById(id);
         if (optionalAccident.isEmpty()) {
             model.addAttribute("error", "Нарушение с указанным "
                     + "идентификатором не найдено");
             return "errors/404";
         }
-        model.addAttribute("accident", optionalAccident);
+        model.addAttribute("accident", optionalAccident.get());
         return "editAccident";
     }
 
-    @PostMapping("/editAccident")
+    @PostMapping("/update")
     public String update(@ModelAttribute Accident accident) {
         accidents.update(accident);
         return "redirect:/index";
