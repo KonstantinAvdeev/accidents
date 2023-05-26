ee4292390fa5aef7692959d1aa38e16c20d84c66
Konstantin Avdeev <fujiun91@gmail.com>
2023-05-26 11:30:51 +0300

    2. Регистрация пользователя [#296069]fix2

--- src/main/java/ru/job4j/accidents/service/SimpleUserService.java
+++ src/main/java/ru/job4j/accidents/service/SimpleUserService.java
@@ -1,6 +1,8 @@
 package ru.job4j.accidents.service;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.job4j.accidents.model.User;
 import ru.job4j.accidents.repository.UserRepository;
@@ -8,17 +10,18 @@ import ru.job4j.accidents.repository.UserRepository;
 @Service
 @AllArgsConstructor
 public class SimpleUserService implements UserService {
+    private static final Logger LOG = LoggerFactory.getLogger(SimpleUserService.class.getName());
     private final UserRepository userRepository;
 
     @Override
     public boolean save(User user) {
         try {
             userRepository.save(user);
+            return true;
         } catch (Exception e) {
-            e.printStackTrace();
-            return false;
+            LOG.error("Что-то пошло не так! Не получается зарегистрировать пользователя!", e);
         }
-        return true;
+        return false;
     }
 
 }
