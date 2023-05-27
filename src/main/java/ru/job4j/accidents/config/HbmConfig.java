a84debea0be776d6d55bc2cca3f98f01a3f381d7
Konstantin Avdeev <fujiun91@gmail.com>
2023-05-27 11:04:27 +0300

    0. Spring test [#6881]fix3

--- src/main/java/ru/job4j/accidents/config/HbmConfig.java
+++ src/main/java/ru/job4j/accidents/config/HbmConfig.java
@@ -1,26 +1,16 @@
 package ru.job4j.accidents.config;
 
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Properties;
 
-@Configuration
-@PropertySource("classpath:db.properties")
-@EnableTransactionManagement
 public class HbmConfig {
 
-    @Bean
-    public LocalSessionFactoryBean sessionFactory(@Value("${hibernate.dialect}") String dialect,
+    public LocalSessionFactoryBean sessionFactory(String dialect,
                                                   DataSource ds) {
         LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
         sessionFactory.setDataSource(ds);
@@ -31,7 +21,6 @@ public class HbmConfig {
         return sessionFactory;
     }
 
-    @Bean
     public PlatformTransactionManager htx(SessionFactory sf) {
         HibernateTransactionManager tx = new HibernateTransactionManager();
         tx.setSessionFactory(sf);
