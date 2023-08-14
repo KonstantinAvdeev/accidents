034becfd6212003c3641dbde7d8541c93ca96812
Konstantin Avdeev <fujiun91@gmail.com>
2023-08-14 20:10:06 +0300

    fix README.md

--- src/main/java/ru/job4j/accidents/config/JdbcConfig.java
+++ src/main/java/ru/job4j/accidents/config/JdbcConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import javax.sql.DataSource;
 
